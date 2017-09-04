2f140a394f8b8f03b7717b5d8fa366af927b0015
kreplay <delih@live.com>
2017-09-03 23:02:43 -0300

    Create demo.js

---
+++ build/demo.js
@@ -0,0 +1,73 @@
+// =============================================== //
+
+// - Remove any active classes (yellow boxes)
+
+// - Add active class to the clicked example box
+
+// - Destroy any pervious Kast
+
+// - On example box click, call Kast
+
+// =============================================== //
+
+
+
+
+
+// Handle: All Example boxes
+
+$('.card-content').click(function () {
+
+    $('.card-content').removeClass('active');
+
+    $(this).addClass('active');
+
+    
+
+    // Destroy any pervious Kast
+
+    // before we call a new Kast
+
+    $.kast('destroy')    
+
+})
+
+
+
+// Handle: Example 3 click function
+
+//$('#demo-3').click(function () {
+
+    $.kast({
+
+        host: 'rosetta.shoutca.st',
+
+        port: 8325,
+        
+        version: 2,
+
+        colors: 'dynamic',
+
+        theme: 'dynamic',
+		
+		autoPlay: true,
+
+        continuous: false,
+
+		played: true,
+
+		minimizeMaximize: true,
+
+		startTemplate: 'minimized',
+
+		betaProxies: true,
+
+		direct: true,		
+
+
+
+    })
+
+//})
+
+
